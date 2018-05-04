5f4c355f1a4dc456f9642e56218204e82a57bf0a
do7be <bomsap700@gmail.com>
2018-05-04 16:59:58 +0900

    Onesignalを追加

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,6 +11,17 @@ const Layout = (props) => (
   <div style={layoutStyle}>
     <Head>
       <link rel="manifest" href="/static/manifest.json"/>
+      <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
+      <script dangerouslySetInnerHTML={{__html: `
+        var OneSignal = window.OneSignal || []
+        if (OneSignal) {
+          OneSignal.push(function() {
+            OneSignal.init({
+              appId: "f64b1892-dc6f-4b3e-8fb2-6268db7128f0",
+            })
+          })
+        }
+      `}}/>
     </Head>
     <Header/>
     {props.children}
