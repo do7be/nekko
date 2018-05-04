1bc7e9600d6749408e07414a8eb2b3466c2af9b7
do7be <bomsap700@gmail.com>
2018-05-04 19:02:25 +0900

    fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,15 +10,16 @@ const layoutStyle = {
 const Layout = (props) => (
   <div style={layoutStyle}>
     <Head>
+      <link rel="apple-touch-icon" href="/static/1.jpg"/>
       <link rel="manifest" href="/static/manifest.json"/>
       <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
       <script dangerouslySetInnerHTML={{__html: `
-        var OneSignal = window.OneSignal || []
+        var OneSignal = window.OneSignal || [];
         OneSignal.push(function() {
           OneSignal.init({
             appId: "f64b1892-dc6f-4b3e-8fb2-6268db7128f0",
           })
-        })
+        });
       `}}/>
     </Head>
     <Header/>
