d43f34b9f51fae5db858171162301b686fcf9515
do7be <bomsap700@gmail.com>
2018-05-04 18:51:02 +0900

    cacheするようにした

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,20 @@
-self.addEventListener('install', function(e) {
+const CACHE_NAME = "simple-cache-v1";
+const urlsToCache = ["/"];
+
+self.addEventListener("install", event => {
+  const preLoaded = caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  event.waitUntil(preLoaded);
   console.log('[ServiceWorker] Install')
-})
+});
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate')
 })
 
-self.addEventListener('fetch', function(event) {})
+self.addEventListener("fetch", event => {
+  const response = caches.match(event.request)
+      .then(match => match || fetch(event.request));
+  event.respondWith(response);
+  console.log('[ServiceWorker] Fetch')
+});
