4308f2c8da25ffcb029476c40cfa4b737a3e60d5
do7be <bomsap700@gmail.com>
2018-05-04 18:14:39 +0900

    OneSignalのファイルをプロキシする

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const next = require('next')
 const routes = require('./routes')
+const { parse } = require('url')
+const { createReadStream } = require('fs')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 
 const { createServer } = require('http')
 app.prepare().then(() => {
-  createServer(handler).listen(process.env.PORT || 3000)
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl
+
+    if (pathname === '/OneSignalSDKWorker.js' || pathname === '/OneSignalSDKUpdaterWorker.js') {
+      res.setHeader('content-type', 'text/javascript')
+      createReadStream(`./static${pathname}`).pipe(res)
+    } else {
+      handler(req, res, parsedUrl)
+    }
+  }).listen(process.env.PORT || 3000)
 })
